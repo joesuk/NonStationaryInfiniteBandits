3e9396159b5cb4bf44c29d64ab9a534d1d0979cd
junghun.kim <junghunkim@kaist.ac.kr>
2025-05-01 17:10:04 +0900

    Add files via upload
    SSUCB-SW and abrupt rotting.

--- Environment.py
+++ Environment.py
@@ -1,4 +1,3 @@
-# infinite-armed bandit environments
 import random
 import numpy as np
 import math
@@ -6,66 +5,63 @@ from numpy.random import seed
 from numpy.random import rand
 from scipy.stats import bernoulli
 from scipy import stats
+import copy
 
 
-class rotting_many_Env: 
-    """
-    slow rotting bandit environment
-    """
+class rotting_many_Env: #slow rotting
     def inverse_cdf(self, y, beta):
-        """
-        analytically compute inverse cdf
-        input:
-            beta: reservoir regularity 
-            y: cdf input
-        """
+    # Computed analytically
         return 1-(1-y)**(1/beta)
 
     def sample_distribution(self,beta):
-        """
-        sample from distribution using inverse cdf
-        input:
-            beta: reservoir regularity 
-        """
         uniform_random_sample = random.random()
         return self.inverse_cdf(uniform_random_sample,beta)
     
-    def __init__(self,rho,seed,T,beta=1):
-        """
-        input:
-            rho: rotting amount
-            seed: random seed
-            T: time horizon
-            beta: reservoir regularity 
-        """
+    # def sample_distribution(self,beta):
+    #     u = np.random.uniform(0, 1, 1)
+    
+    # # Apply the inverse CDF transformation
+    #     mu_sample = 1 - u**(1 / beta)
+    #     # uniform_random_sample = random.random()
+    #     return mu_sample
+    
+    def __init__(self,rho,seed,T,beta=1,L_bool=False):
         np.random.seed(seed)
         self.optimal=1
-        self.exp_reward=[] # mean reward vector
+        self.exp_reward=[]
         self.rho=rho
         self.T=T
         self.beta=beta
+        self.bool=L_bool
+        # if beta==1:
+        #     for k in range(self.T):
+        #         self.exp_reward[k]=np.random.uniform(0,1)
+        # else: 
+        #     for k in range(self.T):
+        #         self.exp_reward[k]=self.sample_distribution(beta)
 
 
     def sample_arm(self):
-        """
-        sample a random arm from reservoir
-        """
         if self.beta==1:
             self.exp_reward.append(np.random.uniform(0,1))
         else: 
             self.exp_reward.append(self.sample_distribution(self.beta))
         
     def observe(self,k,t):
-        """
-        observe round t reward of arm k and update via rotting
-        return:
-            exp_reward: vector of mean rewards
-            reward: observed reward of arm k at this round
-        """
+        # S=round(np.sqrt(self.T))
         gap=1-self.exp_reward[k]
-        reward=self.exp_reward[k]+np.random.uniform(-gap,gap) # observed random reward with variance gap
-        exp_reward=self.exp_reward[k]
-        rho_t=self.rho*(1/(t+1)) # set rotting for this round
+        reward=self.exp_reward[k]+np.random.uniform(-gap,gap)
+        exp_reward=copy.deepcopy(self.exp_reward[k])
+        # if t%S==0:
+        #     self.exp_reward[k]= np.random.uniform(0,1)
+        rho_t=self.rho*(30/(t+1))
+        if self.bool==True:
+            L=int(np.sqrt(self.T))
+            if t<=L:
+                rho_t=1
+            else:
+                rho_t=0
+        # # rho_t=1/10
         self.exp_reward[k]=max(0,self.exp_reward[k]-rho_t)
         return exp_reward, reward
     
--- main_multi.py
+++ main_multi.py
@@ -1,6 +1,5 @@
-# script to run experiments
 from Environment import *
-from Algorithms import *
+from Algorithms_old import *
 import random
 import numpy as np
 import math
@@ -10,15 +9,8 @@ import sys
 from pathlib import Path
 import multiprocessing
 
-def run(T,num,repeat,beta, i, j):
-    """
-    Run bandit experiments
-    parameters:
-        T: horizon
-        num: number of investigated horizon times over maximum time horizon
-        repeat: number of running algorithms using different seeds.
-        beta: reservoir regularity parameter
-    """
+
+def run(T,num,repeat,beta, i, j, L_bool): #regret vs T
     T_1=int(T/num)
     num=num+1
     T_list=np.zeros(num)
@@ -27,7 +19,8 @@ def run(T,num,repeat,beta, i, j):
     regret_sum_list=dict()
     avg_regret_sum=dict()
 
-    alg_list=['Blackbox', 'AUCBT_AW', 'SSUCB','Elimination']
+    alg_list=['algorithm1', 'AUCBT_AW', 'SSUCB','Elimination', 'SSUCB-SW']
+    # alg_list=['SSUCB-SW']
 
     print('num:', i)
     if i==0:
@@ -45,63 +38,52 @@ def run(T,num,repeat,beta, i, j):
         avg_regret_sum[name]=np.zeros(T,float)
     K=int(max(np.sqrt(T),T**(beta/(beta+1))))
     T_list[i]=T
-    regret=np.zeros(T,float) # per-round regrets
-    regret_sum=np.zeros(T,float) # cumulative regret
+    regret=np.zeros(T,float)
+    regret_sum=np.zeros(T,float)
 
-    ## Run model
+###Run model
 
     print('repeat: ',j)
     for name in alg_list:
         print(name)
-        seed=j # random seed
-        Env=rotting_many_Env(rho,seed,T,beta) # make environment
-        if name=='Blackbox':
+        seed=j
+        Env=rotting_many_Env(rho,seed,T,beta,L_bool)
+        if name=='algorithm1':
             algorithm=black_box(T,seed,beta,Env)
         if name=='AUCBT_AW':
             algorithm=AUCBT_AW(T,seed,Env)
         if name=='SSUCB':
             algorithm=SSUCB(K,T,seed,Env)
+        if name=='SSUCB-SW':
+            algorithm=SSUCB_SW(K,T,seed,Env)
         if name=='Elimination':
             algorithm=Elimination(T,seed,beta,Env)
     
-        opti_rewards=Env.optimal # get optimal rewards
+        opti_rewards=Env.optimal
             
-        regret=opti_rewards-algorithm.rewards() # compute regret
-        regret_sum=np.cumsum(regret)  
+        regret=opti_rewards-algorithm.rewards()
+        regret_sum=np.cumsum(regret)
+        
    
-        filename_1=name+'T'+str(T)+'num'+str(i)+'beta'+str(beta)+'repeat'+str(j)+'regret.txt'
+        filename_1=name+'T'+str(T)+'num'+str(i)+'beta'+str(beta)+'repeat'+str(j)+'bool'+str(L_bool)+'regret.txt'
         with open('./result/'+filename_1, 'wb') as f:
             pickle.dump(regret_sum, f)
             f.close()
  
 
-def run_multiprocessing(T,num,repeat, beta):
-    """
-    Run bandit experiments via multi-processing.
-    parameters:
-        T: horizon
-        num: number of investigated horizon times over maximum time horizon
-        repeat: number of running algorithms using different seeds
-        beta: reservoir regularity parameter
-    """
+    
+def run_multiprocessing(T,num,repeat, beta, L_bool):
     Path("./result").mkdir(parents=True, exist_ok=True)
 
     num_processes = multiprocessing.cpu_count()
     with multiprocessing.Pool(processes=num_processes) as pool:
-        pool.starmap(run, [( T,num,repeat,beta, i, j) for j in range(repeat) for i in range(num+1)])
+        pool.starmap(run, [( T,num,repeat,beta, i, j, L_bool) for j in range(repeat) for i in range(num+1)])
 
     pool.close()
     pool.join()
     
 def plot(T,num,repeat, beta):
-    """
-    Plot results of experiments
-    parameters:
-        T: horizon
-        num: number of investigated horizon times over maximum time horizon
-        repeat: number of running algorithms using different seeds
-        beta: reservoir regularity parameter
-    """
+
     T_1=int(T/num)
     num=num+1
 
@@ -115,7 +97,7 @@ def plot(T,num,repeat, beta):
     sd=dict()
     regret_sum_list=dict()
     avg_regret_sum=dict()
-    alg_list=['Blackbox', 'AUCBT_AW', 'SSUCB','Elimination']
+    alg_list=['SSUCB','SSUCB-SW','AUCBT_AW', 'algorithm1', 'Elimination']
     
 
     for name in alg_list:
@@ -136,7 +118,7 @@ def plot(T,num,repeat, beta):
             avg_regret_sum[name]=np.zeros(T,float)
             
             for j in range(repeat):
-                filename_1=name+'T'+str(T)+'num'+str(i)+'beta'+str(beta)+'repeat'+str(j)+'regret.txt'
+                filename_1=name+'T'+str(T)+'num'+str(i)+'beta'+str(beta)+'repeat'+str(j)+'bool'+str(L_bool)+'regret.txt'
  
                 pickle_file1 = open('./result/'+filename_1, "rb")
                 objects = []
@@ -162,15 +144,22 @@ def plot(T,num,repeat, beta):
                 std_list_T[name]=sd[name]
     fig,(ax)=plt.subplots(1,1)
 
-    ax.errorbar(x=T_list, y=regret_list['Blackbox'], yerr=1.96*std_list['Blackbox']/np.sqrt(repeat), color="royalblue", capsize=7,capthick=2, elinewidth=2,linewidth=2,
-                 marker="^", markersize=0,label='Blackbox (Algorithm 1)',zorder=3,ls='-.') 
+
+    # regret_ref_upper_1=[(max(t**((beta+1)/(beta+2))*np.log(t),t**((2)/(3))*np.log(t))) for t in T_list]
+    # regret_ref_upper_2=[(max(t**((beta+1)/(beta+2))*np.log(t),t**((2)/(3))*np.log(t))+max(t**((2*beta+1)/(2*beta+2))*np.log(t),t**((3)/(4))*np.log(t))) for t in T_list]
+    ax.errorbar(x=T_list, y=regret_list['SSUCB-SW'], yerr=1.96*std_list['SSUCB-SW']/np.sqrt(repeat), color="orange", capsize=7,capthick=2,elinewidth=2,linewidth=2,
+                 marker="s", markersize=0,label='SSUCB-SW',zorder=4,ls='-')
+    ax.errorbar(x=T_list, y=regret_list['SSUCB'], yerr=1.96*std_list['SSUCB']/np.sqrt(repeat), color="black", capsize=7,capthick=2,elinewidth=2,linewidth=2,
+                 marker="s", markersize=0,label='SSUCB',zorder=1,ls=':')
     ax.errorbar(x=T_list, y=regret_list['AUCBT_AW'], yerr=1.96*std_list['AUCBT_AW']/np.sqrt(repeat), color="lightseagreen", capsize=7,capthick=2,elinewidth=2,linewidth=2,
                  marker="o", markersize=0,label='AUCBT-AW',zorder=2,ls='--')
-    ax.errorbar(x=T_list, y=regret_list['SSUCB'], yerr=1.96*std_list['SSUCB']/np.sqrt(repeat), color="gray", capsize=7,capthick=2,elinewidth=2,linewidth=2,
-                 marker="s", markersize=0,label='SSUCB',zorder=1,ls=':')
+    ax.errorbar(x=T_list, y=regret_list['algorithm1'], yerr=1.96*std_list['algorithm1']/np.sqrt(repeat), color="royalblue", capsize=7,capthick=2, elinewidth=2,linewidth=2,
+                 marker="^", markersize=0,label='Blackbox (Algorithm 1)',zorder=3,ls='-.') 
     ax.errorbar(x=T_list, y=regret_list['Elimination'], yerr=1.96*std_list['Elimination']/np.sqrt(repeat), color="salmon", capsize=7,capthick=2,elinewidth=2,linewidth=2,
                  marker="s", markersize=0,label='Elimination (Algorithm 2)',zorder=4,ls='-')
+
     Path("./plot").mkdir(parents=True, exist_ok=True)
+
 
 
     #font size
@@ -181,8 +170,8 @@ def plot(T,num,repeat, beta):
     # remove the errorbars in legend
     handles, labels = ax.get_legend_handles_labels()
     handles = [h[0] for h in handles]
-    labels = [ labels[2], labels[1],labels[0], labels[3]]
-    handles=[handles[2],handles[1],handles[0], handles[3]]
+    # labels = [labels[2], labels[1],labels[0], labels[3], handles[4]]
+    # handles=[handles[2],handles[1],handles[0], handles[3], handles[4]]
 
     ax.legend(handles, labels,numpoints=1)
     # plot 
@@ -191,29 +180,102 @@ def plot(T,num,repeat, beta):
 
     plt.xlabel(r'$T$',fontsize=18)
     plt.ylabel(r'$R_T$',fontsize=18)
-    plt.savefig('./plot/T'+str(T)+'num'+str(num)+'beta'+str(beta)+'repeat'+str(repeat)+'.pdf',bbox_inches='tight')
-    #plt.show()
+    plt.savefig('./plot/T'+str(T)+'num'+str(num)+'beta'+str(beta)+'repeat'+str(repeat)+'bool'+str(L_bool)+'.pdf',bbox_inches='tight')
+    plt.show()
     plt.clf()    
+    
+    # fig,(ax)=plt.subplots(1,1)
+    # T_p=int(T/10)
 
+    # ax.errorbar(x=range(T), y=regret_list_T['SSUCB'], yerr=1.96*std_list_T['SSUCB']/np.sqrt(repeat), color="salmon", errorevery=T_p,
+    #              marker="s", markersize=0,label='SSUCB',zorder=1,ls=':')
+    # ax.errorbar(x=range(T), y=regret_list_T['AUCBT_AW'], yerr=1.96*std_list_T['AUCBT_AW']/np.sqrt(repeat), color="lightseagreen", errorevery=T_p,
+    #              marker="o", markersize=0,label='AUCB-AW',zorder=2,ls='--')
+
+      
+    # ax.errorbar(x=range(T), y=regret_list_T['algorithm1'], yerr=1.96*std_list_T['algorithm1']/np.sqrt(repeat), color="royalblue", errorevery=T_p,
+    #              marker="^", markersize=0,label='Blackbox (Algorithm 1)',zorder=3,ls='-') 
+    # ax.errorbar(x=range(T), y=regret_list_T['Elimination'], yerr=1.96*std_list_T['Elimination']/np.sqrt(repeat), color="gray", errorevery=T_p,  
+    #              marker="s", markersize=0,label='Elimination (Algorithm 2)',zorder=1,ls='-.')
+    # # ax.errorbar(x=T_list, y=regret_list4, yerr=1.96*std_list4/np.sqrt(repeat), color="gray", capsize=7,capthick=2,elinewidth=2,linewidth=3,
+    # #              marker="s", markersize=0,label='SSUCB',zorder=1,ls='-.')
+    # # ax.errorbar(x=T_list, y=regret_ref_upper_1, yerr=0, color="lightsteelblue", capsize=7,capthick=2,elinewidth=2,linewidth=3,
+    # #              marker="s", markersize=0,label='Regret upper bound (Alg1)',zorder=1,ls='--')
+    # # ax.errorbar(x=T_list, y=regret_ref_upper_2, yerr=0, color="palegreen", capsize=7,capthick=2,elinewidth=2,linewidth=3,
+    # #              marker="s", markersize=0,label='Regret upper bound (Alg2)',zorder=1,ls='--')  
+
+    # Path("./plot").mkdir(parents=True, exist_ok=True)
+
+
+
+    # #font size
+    # ax.tick_params(labelsize=18)
+    # plt.rc('legend',fontsize=15)
+    # ax.yaxis.get_offset_text().set_fontsize(18)
+    # ax.xaxis.get_offset_text().set_fontsize(18)
+    # # remove the errorbars in legend
+    # handles, labels = ax.get_legend_handles_labels()
+    # handles = [h[0] for h in handles]
+    # labels = [ labels[0], labels[1],labels[2], labels[3]]
+    # handles=[handles[0],handles[1],handles[2], handles[3]]
+
+    # ax.legend(handles, labels,numpoints=1)
+    # # plot 
+    # plt.gcf().subplots_adjust(bottom=0.15)
+    # plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+    # plt.xlabel(r'$T$',fontsize=18)
+    # plt.ylabel(r'$R(T)$',fontsize=18)
+    # plt.savefig('./plot/T'+str(T)+'num'+str(num)+'beta'+str(beta)+'repeat'+str(repeat)+'T.pdf',bbox_inches='tight')
+    # plt.show()
+    # plt.clf()    
             
 if __name__=='__main__':
-
+    # Read input
+    # beta = int(sys.argv[1]) # '1': (left) in Figure 1, '2': (right) in Figure 4
     
-    run_bool=True # True: run model and save data with plot, False: load data with plot.
+    run_bool=True# True: run model and save data with plot, False: load data with plot.
     
+    
+    # if opt==1: 
     T=10**5  # Maximum Time horizon
     num=5 # number of investigated horizon times over maximum time horizon
     repeat=5    # number of running algorithms using different seeds.
-    beta=1
-    if run_bool==True:
-        run_multiprocessing(T,num,repeat, beta)
-    plot(T,num,repeat, beta)    
-    beta=1.2
-    if run_bool==True:
-        run_multiprocessing(T,num,repeat, beta)
-    plot(T,num,repeat, beta) 
     beta=0.8
+    L_bool=False
     if run_bool==True:
-        run_multiprocessing(T,num,repeat, beta)
-    plot(T,num,repeat, beta) 
+        # beta=0.5
+        # run_multiprocessing(T,num,repeat, beta)
+        # plot(T,num,repeat, beta)
+        run_multiprocessing(T,num,repeat, beta, L_bool)
+    plot(T,num,repeat, beta)    
+    # beta=1.2
+    # if run_bool==True:
+    #     # beta=0.5
+    #     # run_multiprocessing(T,num,repeat, beta)
+    #     # plot(T,num,repeat, beta)
+    #     run_multiprocessing(T,num,repeat, beta, L_bool)
+    # plot(T,num,repeat, beta) 
+    # beta=0.8
+    # if run_bool==True:
+    #     beta=0.5
+    #     run_multiprocessing(T,num,repeat, beta, L_bool)
+    # plot(T,num,repeat, beta)
+        # run_multiprocessing(T,num,repeat, beta)
+    # plot(T,num,repeat, beta) 
+        # beta=1.5
+        # run_multiprocessing(T,num,repeat, beta)
+        # plot(T,num,repeat, beta)
 
+    # if opt==2: 
+    #     T=5*10**6  
+    #     num=5 
+    #     repeat=10   
+    #     beta=0.5
+    #     run(T,num,repeat, beta, run_bool)
+    # if opt==3: 
+    #     T=5*10**6  
+    #     num=5 
+    #     repeat=10   
+    #     beta=2
+    #     run(T,num,repeat, beta, run_bool)
